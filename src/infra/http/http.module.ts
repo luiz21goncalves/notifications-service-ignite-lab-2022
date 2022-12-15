042c4a42c5113e0c8ca939e5188c91a9fd2ce36d
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-12-14 21:29:40 -0300

    chore: add notifications controller

---
+++ src/infra/http/http.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+
+import { SendNotification } from '../../application/use-cases/send-notification';
+import { NotificationsController } from './controllers/notifications.controller';
+import { DatabaseModule } from '../database/database.module';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [NotificationsController],
+  providers: [SendNotification],
+})
+export class HttpModule {}
