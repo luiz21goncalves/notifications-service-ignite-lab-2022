f43012f93bb476b5b4d5d286f5075654d07c847c
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-12-16 14:39:29 -0300

    chore: add unread notification use case

---
+++ src/application/use-cases/unread-notification.spec.ts
@@ -0,0 +1,36 @@
+import { describe, it } from '@jest/globals';
+
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { makeNotification } from '@test/factories/notification-factory';
+import { NotificationNotFound } from './errors/notification-not-found';
+import { UnreadNotification } from './unread-notification';
+
+describe('Read notification', () => {
+  it('should be able to unread a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const unreadNotification = new UnreadNotification(notificationsRepository);
+
+    const notification = makeNotification({
+      readAt: new Date(),
+    });
+
+    await notificationsRepository.create(notification);
+
+    await unreadNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications.at(0)?.readAt).toBeNull();
+  });
+
+  it('should not be able to unread a non existing notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const unreadNotification = new UnreadNotification(notificationsRepository);
+
+    expect(() =>
+      unreadNotification.execute({
+        notificationId: 'fake-notification-id',
+      }),
+    ).rejects.toThrow(NotificationNotFound);
+  });
+});
